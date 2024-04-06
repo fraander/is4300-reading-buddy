7e3348299fd7a29afedd75d28039470d9adb48cc
fraander <57777918+fraander@users.noreply.github.com>
2024-04-06 15:09:45 -0400

    Update ChatGpt.tsx

--- src/components/reused/ChatGpt.tsx
+++ src/components/reused/ChatGpt.tsx
@@ -2,11 +2,6 @@ import OpenAI from "openai";
 import { useState } from "react";
 import VDiv from "../utils/VDiv";
 
-const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI,
-  dangerouslyAllowBrowser: true,
-});
-
 export default function ChatGpt() {
   const [message, setMessage] = useState<string>("Pet me for motivation!");
   const [dog, setDog] = useState<number>(0);
@@ -21,20 +16,43 @@ export default function ChatGpt() {
 
   const fetch = async () => {
     setMessage(() => "");
-    console.log("calling");
-    const prompt: string =
-      "Write me a very short (not more than 10 words) motivational message to help me, a young child and beginner reader, want to keep reading!";
-    const stream = await openai.chat.completions.create({
-      model: "gpt-4",
-      messages: [{ role: "user", content: prompt }],
-      stream: true,
-    });
-    console.log("responding");
-    for await (const chunk of stream) {
-      const output = chunk.choices[0]?.delta?.content || "";
-      console.log(message + output);
-      setMessage((current) => current + output.replace(/"/g, ""));
+
+    if (import.meta.env.VITE_OPENAI) {
+      // If the OpenAI ENV is setup, use it ...
+      const openai = new OpenAI({
+        apiKey: import.meta.env.VITE_OPENAI,
+        dangerouslyAllowBrowser: true,
+      });
+
+      console.log("calling");
+      const prompt: string =
+        "Write me a very short (not more than 10 words) motivational message to help me, a young child and beginner reader, want to keep reading!";
+      const stream = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [{ role: "user", content: prompt }],
+        stream: true,
+      });
+      console.log("responding");
+      for await (const chunk of stream) {
+        const output = chunk.choices[0]?.delta?.content || "";
+        console.log(message + output);
+        setMessage((current) => current + output.replace(/"/g, ""));
+      }
+    } else {
+      // Otherwise, pretend to use it ...
+      setTimeout(() => {
+        const messages = [
+          "Keep reading, you can do it!",
+          "Just a few more pages to go!",
+          "Stay focused, I know it's not always easy!",
+        ];
+
+        setMessage(messages[Math.floor(Math.random() * messages.length)]);
+      }, 2000);
     }
+
+    // Either way, update the dog ...
+
     setDog(Math.floor(Math.random() * dogRoutes.length));
     console.log("completed");
   };
@@ -43,7 +61,9 @@ export default function ChatGpt() {
     <VDiv onClick={() => fetch()}>
       <img className="h-64 w-64 p-4 brightness" src={dogRoutes[dog]} />
 
-      <div className="p-4 mx-4 mb-4 text-lg font-serif font-medium bg-white rounded-full text-center">{message}</div>
+      <div className="p-4 mx-4 mb-4 text-lg font-serif font-medium bg-white rounded-full text-center">
+        {message}
+      </div>
     </VDiv>
   );
 }
