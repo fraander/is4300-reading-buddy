d6f509129d92dca25344badfb37338a151db58c2
Frank Anderson <57777918+fraander@users.noreply.github.com>
2024-04-11 21:55:49 -0400

    Update Progress.tsx to possibly be leading aligned

--- src/components/reused/Progress.tsx
+++ src/components/reused/Progress.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function Progress({ current, full }: Props) {
   return (
-    <div className={`bg-rbp-500 h-2.5 rounded-full`}
+    <div className={`bg-rbp-500 h-2.5 rounded-full mr-auto`}
       style={{ width: `${(current / full) * 100}%` }}>
       <div
         className="w-full bg-rbp-0 rounded-full h-2.5 dark:bg-rbp-700"
